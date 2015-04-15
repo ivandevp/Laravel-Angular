f4a3a25fbf7d167fd04737e9680e41e884172aca
Iván Medina Díaz <medinadiazivan@gmail.com>
2015-04-15 17:18:58 -0500

    View - index.php
    Integration with AngularJS

--- public/js/services/commentService.js
+++ public/js/services/commentService.js
@@ -7,14 +7,14 @@ angular.module('commentService', [])
 	return {
 		// get all the comments
 		get : function() {
-			return $http.get('/api/comments');
+			return $http.get('/api/comments/');
 		},
 
 		// save a comment (pass in comment data)
 		save : function(commentData) {
 			return $http({
 				method: 'POST',
-				url: '/api/comments',
+				url: '/api/comments/',
 				headers: { 'Content-Type' : 'application/x-www-form-urlencoded'},
 				data: $.param(commentData)
 			});
