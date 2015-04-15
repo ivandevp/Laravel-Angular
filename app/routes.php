2bbe5d4e884d3ac690fdbd4a8f2b45c06aac0657
Iván Medina Díaz <medinadiazivan@gmail.com>
2015-04-15 16:45:43 -0500

    Routes
    php artisan routes

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,32 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
+
+We don't need to use Laravel Blade
+We will return a PHP file that will hold all of our Angular content
+See the "Where to Place Angular Files" below to see ideas on how to structure the return
+
 */
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	return View::make('index'); // will return app/views/index.php
+});
+
+// API ROUTES
+
+Route::group(array('prefix' => 'api'), function() {
+	// since we will be using this just for CRUD, we won't need to create and edit
+	// Angular will handle both of those forms
+	// this ensures that a user can't access api/create or api/edit when there's nothing there
+	Route::resource('comments', 'CommentController',
+		array('only' => array('index','store','destroy')));
+});
+
+// CATCH ALL ROUTE
+// all routes that are not home or api will be redirected to the frontend
+// this allows angular to route them
+
+App::missing(function($exception) {
+	return View::make('index');
 });
